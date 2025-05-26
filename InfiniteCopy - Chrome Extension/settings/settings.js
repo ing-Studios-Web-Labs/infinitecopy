8963ea81e62e947364f2ae8c3ef85cf240695ed7
ingStudiosOfficial <pluscompanyhqDEPT@gmail.com>
2025-05-26 17:16:45 +0800

    Delete InfiniteCopy - Chrome Extension directory

--- InfiniteCopy - Chrome Extension/settings/settings.js
+++
@@ -1,6 +0,0 @@
-function displayFooterText() {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const footerText = document.getElementById('footer-text');
-    footerText.textContent = `©${currentYear} (ing) Studios Web Labs`;
-}
